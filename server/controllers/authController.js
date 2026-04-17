362ac630285ba62df116c87acb679f2755e0f3d4
Mohammed Kawaf <mohammedkawaf@MacBook-Air-som-tillhor-Mohammed.local>
2026-04-17 15:14:14 +0200

    move login to MVC structure (auth controller and routes)

---
+++ server/controllers/authController.js
@@ -0,0 +1,17 @@
+const db = require("../models/db");
+
+exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  const query = "SELECT * FROM users WHERE email = ? AND password = ?";
+
+  db.query(query, [email, password], (err, results) => {
+    if (err) return res.status(500).json(err);
+
+    if (results.length > 0) {
+      res.json({ success: true, user: results[0] });
+    } else {
+      res.json({ success: false });
+    }
+  });
+};
