0aa0b66010f149a356bd3a31c7241afbdea89496
Mahmoud Nour <m1nour@MacBook-Air-som-tillhor-Mahmoud.local>
2026-05-28 11:40:41 +0200

    fix: Use environment API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API = import.meta.env.VITE_API_URL || "http://localhost:3030";
+const API = import.meta.env.VITE_API_URL;
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem("token");
